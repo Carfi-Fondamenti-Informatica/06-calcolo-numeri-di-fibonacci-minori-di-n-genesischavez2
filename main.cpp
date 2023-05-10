97006166f085eb33b7875b315476bbab6ab713bf
genesischavez2 <127029593+genesischavez2@users.noreply.github.com>
2023-05-10 19:30:54 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,16 +4,17 @@ using namespace std;
 int main() {
    int n=0,m=1,y=1,x=0;
    cin>>n;
+   cout <<"1"<<endl; 
    for(int i=1;i<=n;i++){
       if(y<=n){
-         cout<<y<<endl;
          y=m+x;
+         cout<<y<<endl;
+      }  else{
+       break;
+       }  
          x=m;
          m=y;
-      } else{
-       break;
-       }
-   }  
+     }  
          
    return 0;
 }
